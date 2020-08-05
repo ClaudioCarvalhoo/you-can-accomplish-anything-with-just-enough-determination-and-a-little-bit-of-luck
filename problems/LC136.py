f8c2cd094a9f271418e264c0bf1cff30f98a3968
Claudio Carvalho <carvalhooclaudio@gmail.com>
2020-08-05 02:06:25 -0300

    feat: LC136

---
+++ problems/LC136.py
@@ -0,0 +1,27 @@
+# O(n)
+# n = len(nums)
+
+# Normal solution
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        table = {}
+        for i in nums:
+            if i in table:
+                table[i] += 1
+            else:
+                table[i] = 1
+        
+        for i in table.keys():
+            if table[i] == 1:
+                return i
+
+# This has O(1) space complexity
+# Xor between a number and itself = 0
+# Xor between 0 and n = n
+# All numbers cancell eachother until only n remains in the end
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for i in nums:
+            res ^= i
+        return res
