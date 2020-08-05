c2567fda57984219f85e9865ea49988ffddb97af
Claudio Carvalho <carvalhooclaudio@gmail.com>
2020-08-05 19:11:38 -0300

    feat: LC991

---
+++ problems/LC991.py
@@ -0,0 +1,16 @@
+# O(log(Y))
+
+class Solution:
+    def brokenCalc(self, X: int, Y: int) -> int:
+        if X > Y:
+            return X - Y
+        
+        count = 0
+        while X < Y:
+            count += 1
+            if Y % 2 == 0:
+                Y /= 2
+            else:
+                Y += 1
+                
+        return int(count + X-Y)
