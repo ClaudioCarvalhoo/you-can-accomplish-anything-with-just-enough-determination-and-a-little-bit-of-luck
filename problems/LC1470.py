cdf8e7eccf3705e3d0e7969bba8e798cba1ee9d2
Claudio Carvalho <carvalhooclaudio@gmail.com>
2020-08-04 23:29:26 -0300

    feat: LC1470

---
+++ problems/LC1470.py
@@ -0,0 +1,11 @@
+# O(n)
+# n = n
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res = []
+        for i in range(n):
+            res.append(nums[i])
+            res.append(nums[n+i])
+            
+        return res
