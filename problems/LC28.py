3b96175badfcfabe55a8cc1de7a61c1dc9e06752
Claudio Carvalho <cco2@cin.ufpe.br>
2020-08-04 21:59:34 -0300

    feat: LC28

---
+++ problems/LC28.py
@@ -0,0 +1,14 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle) == 0:
+            return 0
+        
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i] == needle[0]:
+                for j in range(len(needle)):
+                    if not haystack[i+j] == needle[j]:
+                        break
+                    elif j == len(needle) -1:
+                        return i
+        
+        return -1
