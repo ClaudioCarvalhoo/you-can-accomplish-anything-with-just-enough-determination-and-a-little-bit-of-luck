257b3741a6dacbdd83d4cf3bfef48b3f8d6eefcd
Claudio Carvalho <carvalhooclaudio@gmail.com>
2020-10-19 23:56:47 -0300

    fix: import List

--- problems/LC136.py
+++ problems/LC136.py
@@ -1,6 +1,8 @@
 # O(n)
 # n = len(nums)
 
+from typing import List
+
 # Normal solution
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
@@ -10,16 +12,17 @@ class Solution:
                 table[i] += 1
             else:
                 table[i] = 1
-        
+
         for i in table.keys():
             if table[i] == 1:
                 return i
+
 
 # This has O(1) space complexity
 # Xor between a number and itself = 0
 # Xor between 0 and n = n
 # All numbers cancell eachother until only n remains in the end
-class Solution:
+class Solution2:
     def singleNumber(self, nums: List[int]) -> int:
         res = 0
         for i in nums:
