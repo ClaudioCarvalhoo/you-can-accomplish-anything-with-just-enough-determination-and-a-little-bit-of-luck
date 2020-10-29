d4be2e67988a7f2b3dc3c66a3f775880e7fcc19f
Claudio Carvalho <carvalhooclaudio@gmail.com>
2020-10-29 18:35:02 -0300

    feat: LC55

---
+++ problems/LC55.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        lastGood = len(nums) - 1
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= lastGood:
+                lastGood = i
+
+        return lastGood == 0
