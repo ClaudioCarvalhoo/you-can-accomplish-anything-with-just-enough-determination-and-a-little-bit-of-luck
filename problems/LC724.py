fff9b5291d55d6cc671391d026bd26e2cb8ce814
Claudio Carvalho <carvalhooclaudio@gmail.com>
2021-04-30 16:52:39 -0300

    feat: LC724

---
+++ problems/LC724.py
@@ -0,0 +1,14 @@
+# O(n)
+# n = len(nums)
+
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        leftSum = 0
+        rightSum = sum(nums)
+        for i in range(len(nums)):
+            rightSum -= nums[i]
+            if leftSum == rightSum:
+                return i
+            leftSum += nums[i]
+        return -1
