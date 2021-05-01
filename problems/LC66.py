82100f66ecb690f71175eabfe0b9e27b1e2cb110
Claudio Carvalho <carvalhooclaudio@gmail.com>
2021-05-01 20:10:33 -0300

    feat: LC66

---
+++ problems/LC66.py
@@ -0,0 +1,17 @@
+# O(n)
+# n = len(digits)
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+        digits[0] = 1
+        for i in range(1, len(digits)):
+            digits[i] = 0
+        digits.append(0)
+        return digits
