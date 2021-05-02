daa40cae59614ce6c5558d3c07ba987b3116e7f0
Claudio Carvalho <carvalhooclaudio@gmail.com>
2021-05-02 20:27:59 -0300

    feat: LC122

--- problems/LC122.py
+++ problems/LC122.py
@@ -15,3 +15,16 @@ class Solution:
             valueBought = prices[i]
 
         return profit
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        i = 0
+        while i < len(prices):
+            boughtFor = prices[i]
+            while i < len(prices) - 1 and prices[i] < prices[i + 1]:
+                i += 1
+            profit += max(0, prices[i] - boughtFor)
+            i += 1
+        return profit
