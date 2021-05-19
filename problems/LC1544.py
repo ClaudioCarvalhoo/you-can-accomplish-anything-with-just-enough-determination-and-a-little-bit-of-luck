29e9957a0a29e8279892599ad67486ff5c88d5ff
Claudio Carvalho <carvalhooclaudio@gmail.com>
2021-05-19 20:18:59 -0300

    feat: LC1544

---
+++ problems/LC1544.py
@@ -0,0 +1,15 @@
+# O(n)
+# n = len(s)
+
+
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack = []
+        for i in range(len(s)):
+            if len(stack) <= 0:
+                stack.append(s[i])
+            elif abs(ord(stack[-1]) - ord(s[i])) == 32:
+                stack.pop()
+            else:
+                stack.append(s[i])
+        return "".join(stack)
