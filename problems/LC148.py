8e380536a5ded37b455de18bfab3d1c5d9302184
Claudio Carvalho <carvalhooclaudio@gmail.com>
2021-06-22 17:11:15 -0300

    refactor: LC148

--- problems/LC148.py
+++ problems/LC148.py
@@ -8,57 +8,54 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: ListNode) -> ListNode:
-        listLen = 0
-        cur = head
-        while cur is not None:
-            cur = cur.next
-            listLen += 1
-
-        if listLen == 0 or listLen == 1:
+        listLen = self.findListLen(head)
+        if listLen <= 1:
             return head
 
-        head = ListNode(None, head)
-        chunkLen = 1
-        while chunkLen < listLen:
-            prev = head
-            remaining = listLen
-            while prev.next is not None:
-                l1ChunkLen = min(chunkLen, remaining)
-                l2ChunkLen = min(chunkLen, remaining - l1ChunkLen)
-                l1 = prev.next
-                l2 = prev.next
-                for _ in range(l1ChunkLen):
-                    l2 = l2.next
-                chunkEnd = self.mergeInPlace(prev, l1, l1ChunkLen, l2, l2ChunkLen)
-                prev = chunkEnd
-                remaining -= l1ChunkLen + l2ChunkLen
-            chunkLen *= 2
-        return head.next
+        preHead = ListNode(float("-inf"), head)
+        self.mergeSort(preHead, listLen)
+        return preHead.next
 
-    def mergeInPlace(self, prev, l1, l1ChunkLen, l2, l2ChunkLen):
-        if l1 == l2:
-            return l1
-        chunkEnd = l2
-        for _ in range(l2ChunkLen):
-            chunkEnd = chunkEnd.next
-        l1Cur, l2Cur = 0, 0
-        while l1Cur < l1ChunkLen or l2Cur < l2ChunkLen:
-            if l1Cur >= l1ChunkLen:
-                prev.next = l2
-                l2 = l2.next
-                l2Cur += 1
-            elif l2Cur >= l2ChunkLen:
-                prev.next = l1
-                l1 = l1.next
-                l1Cur += 1
-            elif l1.val <= l2.val:
-                prev.next = l1
-                l1 = l1.next
-                l1Cur += 1
+    def findListLen(self, head):
+        listLen = 0
+        node = head
+        while node is not None:
+            listLen += 1
+            node = node.next
+        return listLen
+
+    def mergeSort(self, prev, listLen):
+        if listLen == 1:
+            return prev.next
+        leftSize = math.floor(listLen / 2)
+        rightSize = math.ceil(listLen / 2)
+        midPrev = self.mergeSort(prev, leftSize)
+        end = self.mergeSort(midPrev, rightSize).next
+        return self.mergeInPlace(prev, leftSize, midPrev, rightSize, end)
+
+    def mergeInPlace(self, leftPrev, leftSize, rightPrev, rightSize, end):
+        prev = leftPrev
+        left = leftPrev.next
+        right = rightPrev.next
+        while leftSize > 0 and rightSize > 0:
+            if left.val <= right.val:
+                prev.next = left
+                leftSize -= 1
+                left = left.next
             else:
-                prev.next = l2
-                l2 = l2.next
-                l2Cur += 1
+                prev.next = right
+                rightSize -= 1
+                right = right.next
             prev = prev.next
-        prev.next = chunkEnd
+        while leftSize > 0:
+            prev.next = left
+            leftSize -= 1
+            prev = prev.next
+            left = left.next
+        while rightSize > 0:
+            prev.next = right
+            rightSize -= 1
+            prev = prev.next
+            right = right.next
+        prev.next = end
         return prev
